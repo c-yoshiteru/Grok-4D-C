e8137c5e65f8824eaf1dbd0beb97f268d60c0bd7
c-yoshiteru <yoshiteru652@gmail.com>
2025-12-07 12:28:52 +0900

    4dc_true_mari.py の更新

--- 4dc_true_mari.py
+++ 4dc_true_mari.py
@@ -93,7 +93,7 @@ while True:
 
 MIT License
 
-Copyright (c) 2025 よしてる×Grok
+Copyright (c) 2025 よしてる
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
